63f3f44af37395c070f54174dc560d89557c3d3b
moringanepal <moringanepal2081@gmail.com>
2024-10-12 22:11:33 +0545

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach((accordion, index) => {
+  const header = accordion.querySelector(".accordion__header");
+  const content = accordion.querySelector(".accordion__content");
+  const icon = accordion.querySelector("#accordion-icon");
+
+  header.addEventListener("click", () => {
+    const isOpen = content.style.height === `${content.scrollHeight}px`;
+
+    accordions.forEach((a, i) => {
+      const c = a.querySelector(".accordion__content");
+      const ic = a.querySelector("#accordion-icon");
+
+      c.style.height = i === index && !isOpen ? `${c.scrollHeight}px` : "0px";
+      ic.classList.toggle("ri-add-line", i !== index || !isOpen);
+      ic.classList.toggle("ri-subtract-fill", i === index && !isOpen);
+    });
+  });
+});
+(function () {
+
+    const link = document.querySelectorAll('nav > .link');
+    const cursor = document.querySelector('.cursor');
+
+    const animateit = function (e) {
+          const span = this.querySelector('.link > span');
+          const { offsetX: x, offsetY: y } = e,
+          { offsetWidth: width, offsetHeight: height } = this,
+
+          move = 25,
+          xMove = x / width * (move * 2) - move,
+          yMove = y / height * (move * 2) - move;
+
+          span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+          if (e.type === 'mouseleave') span.style.transform = '';
+    };
+
+    const editCursor = e => {
+          const { clientX: x, clientY: y } = e;
+          cursor.style.left = x + 'px';
+          cursor.style.top = y + 'px';
+    };
+
+    link.forEach(b => b.addEventListener('mousemove', animateit));
+    link.forEach(b => b.addEventListener('mouseleave', animateit));
+    window.addEventListener('mousemove', editCursor);
+
+})();
+
